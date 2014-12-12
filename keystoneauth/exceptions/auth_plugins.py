3e07bd2c40fcc05a6ad992aaa3668ced0471940b
Jamie Lennox <jamielennox@redhat.com>
2014-12-12 11:55:02 +1000

    Add get_communication_params interface to plugins
    To allow authentication plugins such as using client certificates or
    doing kerberos authentication with every request we need a way for the
    plugins to manipulate the send parameters.
    
    Change-Id: Ib9e81773ab988ea05869bc27097d2b25e963e59c
    Blueprint: generic-plugins
    (cherry picked from commit 0ecf9b1ab5177fc42d16b4a57e8522769433b156)

--- keystoneauth/exceptions/auth_plugins.py
+++ keystoneauth/exceptions/auth_plugins.py
@@ -36,3 +36,20 @@ class NoMatchingPlugin(AuthPluginException):
         self.name = name
         msg = 'The plugin %s could not be found' % name
         super(NoMatchingPlugin, self).__init__(msg)
+
+
+class UnsupportedParameters(AuthPluginException):
+    """A parameter that was provided or returned is not supported.
+
+    :param list(str) names: Names of the unsupported parameters.
+
+    .. py:attribute:: names
+
+        Names of the unsupported parameters.
+    """
+
+    def __init__(self, names):
+        self.names = names
+
+        m = 'The following parameters were given that are unsupported: %s'
+        super(UnsupportedParameters, self).__init__(m % ', '.join(self.names))
