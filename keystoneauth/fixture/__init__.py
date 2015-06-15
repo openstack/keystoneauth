openstack/python-keystoneclient

Cleanup fixture imports
OpenStack hacking guidelines say to not import objects, only
modules[1].

[1] http://docs.openstack.org/developer/hacking/#imports

Change-Id: I6ccb7fda5c406458567bd20ccc1d673fca245191

keystoneclient/fixture/__init__.py
@@ -22,10 +22,15 @@ testing.
 """
 
 from keystoneclient.fixture.discovery import *  # noqa
-from keystoneclient.fixture.exception import FixtureValidationError  # noqa
-from keystoneclient.fixture.v2 import Token as V2Token  # noqa
-from keystoneclient.fixture.v3 import Token as V3Token  # noqa
-from keystoneclient.fixture.v3 import V3FederationToken  # noqa
+from keystoneclient.fixture import exception
+from keystoneclient.fixture import v2
+from keystoneclient.fixture import v3
+
+
+FixtureValidationError = exception.FixtureValidationError
+V2Token = v2.Token
+V3Token = v3.Token
+V3FederationToken = v3.V3FederationToken
 
 __all__ = ['DiscoveryList',
            'FixtureValidationError',

openstack/keystoneauth

keystoneauth/fixture/__init__.py
@@ -22,10 +22,15 @@ testing.
 """
 
 from keystoneauth.fixture.discovery import *  # noqa
-from keystoneauth.fixture.exception import FixtureValidationError  # noqa
-from keystoneauth.fixture.v2 import Token as V2Token  # noqa
-from keystoneauth.fixture.v3 import Token as V3Token  # noqa
-from keystoneauth.fixture.v3 import V3FederationToken  # noqa
+from keystoneauth.fixture import exception
+from keystoneauth.fixture import v2
+from keystoneauth.fixture import v3
+
+
+FixtureValidationError = exception.FixtureValidationError
+V2Token = v2.Token
+V3Token = v3.Token
+V3FederationToken = v3.V3FederationToken
 
 __all__ = ['DiscoveryList',
            'FixtureValidationError',