2a80dc5f10376886adf483336c207eb1918b0368
Monty Taylor <mordred@inaugust.com>
2015-08-16 11:57:53 +0900

    Port in the argument scrubbing from OCC
    Processing a parameter dict and extracting the declared parameters for
    the given auth plugin is clearly in the problem domain of keystoneauth.
    It's also just simple data transformations. Move it here.
    
    Change-Id: I6da32fee4061ab31207cb06503ec78bb45629fb2

--- keystoneauth1/exceptions/auth_plugins.py
+++ keystoneauth1/exceptions/auth_plugins.py
@@ -53,3 +53,30 @@ class UnsupportedParameters(AuthPluginException):
 
         m = 'The following parameters were given that are unsupported: %s'
         super(UnsupportedParameters, self).__init__(m % ', '.join(self.names))
+
+
+class MissingRequiredParameters(AuthPluginException):
+    """One or more required parameters were not provided.
+
+    :param string name: Name of the missing parameters.
+    :param list(str) parameters: Name of the missing parameters.
+
+    .. py:attribute:: plugin
+
+        Plugin class that was attempted to load.
+
+    .. py:attribute:: parameters
+
+        List of the missing parameters.
+    """
+
+    def __init__(self, plugin, parameters):
+        self.plugin = plugin
+        self.parameters = parameters
+
+        m = ('Auth plugin %(plugin)s requires parameters'
+             ' which were not given: %(parameters)s')
+        super(UnsupportedParameters, self).__init__(
+            m % dict(
+                plugin=self.plugin.plugin_class.__name__,
+                parameters=', '.join(self.parameters)))
