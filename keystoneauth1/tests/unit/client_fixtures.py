68fde05dee456e5df11b334526c1bed37088953d
Raildo Mascena <raildo@lsd.ufcg.edu.br>
2016-04-29 13:00:33 -0500

    Refactor variables for fixture and service
    There is a couple of f and s variables to represent
    fixture and service objects, we must use a better name
    for it.
    
    Change-Id: I3e3e656193d1c0304b08a6ebb94441db489a820d

--- keystoneauth1/tests/unit/client_fixtures.py
+++ keystoneauth1/tests/unit/client_fixtures.py
@@ -10,103 +10,109 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from keystoneauth1 import fixture
+from keystoneauth1 import fixture as kfixture
 
 
 def project_scoped_token():
-    f = fixture.V3Token(user_id='c4da488862bd435c9e6c0275a0d0e49a',
-                        user_name='exampleuser',
-                        user_domain_id='4e6893b7ba0b4006840c3845660b86ed',
-                        user_domain_name='exampledomain',
-                        expires='2010-11-01T03:32:15-05:00',
-                        project_id='225da22d3ce34b15877ea70b2a575f58',
-                        project_name='exampleproject',
-                        project_domain_id='4e6893b7ba0b4006840c3845660b86ed',
-                        project_domain_name='exampledomain')
+    fixture = kfixture.V3Token(
+        user_id='c4da488862bd435c9e6c0275a0d0e49a',
+        user_name='exampleuser',
+        user_domain_id='4e6893b7ba0b4006840c3845660b86ed',
+        user_domain_name='exampledomain',
+        expires='2010-11-01T03:32:15-05:00',
+        project_id='225da22d3ce34b15877ea70b2a575f58',
+        project_name='exampleproject',
+        project_domain_id='4e6893b7ba0b4006840c3845660b86ed',
+        project_domain_name='exampledomain')
 
-    f.add_role(id='76e72a', name='admin')
-    f.add_role(id='f4f392', name='member')
+    fixture.add_role(id='76e72a', name='admin')
+    fixture.add_role(id='f4f392', name='member')
 
     region = 'RegionOne'
     tenant = '225da22d3ce34b15877ea70b2a575f58'
 
-    s = f.add_service('volume')
-    s.add_standard_endpoints(public='http://public.com:8776/v1/%s' % tenant,
-                             internal='http://internal:8776/v1/%s' % tenant,
-                             admin='http://admin:8776/v1/%s' % tenant,
-                             region=region)
+    service = fixture.add_service('volume')
+    service.add_standard_endpoints(
+        public='http://public.com:8776/v1/%s' % tenant,
+        internal='http://internal:8776/v1/%s' % tenant,
+        admin='http://admin:8776/v1/%s' % tenant,
+        region=region)
 
-    s = f.add_service('image')
-    s.add_standard_endpoints(public='http://public.com:9292/v1',
-                             internal='http://internal:9292/v1',
-                             admin='http://admin:9292/v1',
-                             region=region)
+    service = fixture.add_service('image')
+    service.add_standard_endpoints(public='http://public.com:9292/v1',
+                                   internal='http://internal:9292/v1',
+                                   admin='http://admin:9292/v1',
+                                   region=region)
 
-    s = f.add_service('compute')
-    s.add_standard_endpoints(public='http://public.com:8774/v2/%s' % tenant,
-                             internal='http://internal:8774/v2/%s' % tenant,
-                             admin='http://admin:8774/v2/%s' % tenant,
-                             region=region)
+    service = fixture.add_service('compute')
+    service.add_standard_endpoints(
+        public='http://public.com:8774/v2/%s' % tenant,
+        internal='http://internal:8774/v2/%s' % tenant,
+        admin='http://admin:8774/v2/%s' % tenant,
+        region=region)
 
-    s = f.add_service('ec2')
-    s.add_standard_endpoints(public='http://public.com:8773/services/Cloud',
-                             internal='http://internal:8773/services/Cloud',
-                             admin='http://admin:8773/services/Admin',
-                             region=region)
+    service = fixture.add_service('ec2')
+    service.add_standard_endpoints(
+        public='http://public.com:8773/services/Cloud',
+        internal='http://internal:8773/services/Cloud',
+        admin='http://admin:8773/services/Admin',
+        region=region)
 
-    s = f.add_service('identity')
-    s.add_standard_endpoints(public='http://public.com:5000/v3',
-                             internal='http://internal:5000/v3',
-                             admin='http://admin:35357/v3',
-                             region=region)
+    service = fixture.add_service('identity')
+    service.add_standard_endpoints(public='http://public.com:5000/v3',
+                                   internal='http://internal:5000/v3',
+                                   admin='http://admin:35357/v3',
+                                   region=region)
 
-    return f
+    return fixture
 
 
 def domain_scoped_token():
-    f = fixture.V3Token(user_id='c4da488862bd435c9e6c0275a0d0e49a',
-                        user_name='exampleuser',
-                        user_domain_id='4e6893b7ba0b4006840c3845660b86ed',
-                        user_domain_name='exampledomain',
-                        expires='2010-11-01T03:32:15-05:00',
-                        domain_id='8e9283b7ba0b1038840c3842058b86ab',
-                        domain_name='anotherdomain')
+    fixture = kfixture.V3Token(
+        user_id='c4da488862bd435c9e6c0275a0d0e49a',
+        user_name='exampleuser',
+        user_domain_id='4e6893b7ba0b4006840c3845660b86ed',
+        user_domain_name='exampledomain',
+        expires='2010-11-01T03:32:15-05:00',
+        domain_id='8e9283b7ba0b1038840c3842058b86ab',
+        domain_name='anotherdomain')
 
-    f.add_role(id='76e72a', name='admin')
-    f.add_role(id='f4f392', name='member')
+    fixture.add_role(id='76e72a', name='admin')
+    fixture.add_role(id='f4f392', name='member')
     region = 'RegionOne'
 
-    s = f.add_service('volume')
-    s.add_standard_endpoints(public='http://public.com:8776/v1/None',
-                             internal='http://internal.com:8776/v1/None',
-                             admin='http://admin.com:8776/v1/None',
-                             region=region)
+    service = fixture.add_service('volume')
+    service.add_standard_endpoints(public='http://public.com:8776/v1/None',
+                                   internal='http://internal.com:8776/v1/None',
+                                   admin='http://admin.com:8776/v1/None',
+                                   region=region)
 
-    s = f.add_service('image')
-    s.add_standard_endpoints(public='http://public.com:9292/v1',
-                             internal='http://internal:9292/v1',
-                             admin='http://admin:9292/v1',
-                             region=region)
+    service = fixture.add_service('image')
+    service.add_standard_endpoints(public='http://public.com:9292/v1',
+                                   internal='http://internal:9292/v1',
+                                   admin='http://admin:9292/v1',
+                                   region=region)
 
-    s = f.add_service('compute')
-    s.add_standard_endpoints(public='http://public.com:8774/v1.1/None',
-                             internal='http://internal:8774/v1.1/None',
-                             admin='http://admin:8774/v1.1/None',
-                             region=region)
+    service = fixture.add_service('compute')
+    service.add_standard_endpoints(public='http://public.com:8774/v1.1/None',
+                                   internal='http://internal:8774/v1.1/None',
+                                   admin='http://admin:8774/v1.1/None',
+                                   region=region)
 
-    s = f.add_service('ec2')
-    s.add_standard_endpoints(public='http://public.com:8773/services/Cloud',
-                             internal='http://internal:8773/services/Cloud',
-                             admin='http://admin:8773/services/Admin',
-                             region=region)
+    service = fixture.add_service('ec2')
+    service.add_standard_endpoints(
+        public='http://public.com:8773/services/Cloud',
+        internal='http://internal:8773/services/Cloud',
+        admin='http://admin:8773/services/Admin',
+        region=region)
 
-    s = f.add_service('identity')
-    s.add_standard_endpoints(public='http://public.com:5000/v3',
-                             internal='http://internal:5000/v3',
-                             admin='http://admin:35357/v3',
-                             region=region)
+    service = fixture.add_service('identity')
+    service.add_standard_endpoints(public='http://public.com:5000/v3',
+                                   internal='http://internal:5000/v3',
+                                   admin='http://admin:35357/v3',
+                                   region=region)
 
-    return f
+    return fixture
 
 
 AUTH_SUBJECT_TOKEN = '3e2813b7ba0b4006840c3825860b86ed'
