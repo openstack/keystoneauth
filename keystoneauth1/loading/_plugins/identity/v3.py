5623c0191895a2b6b27a1c85d6f8d728a751c519
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2016-05-23 10:01:43 +0200

    oidc: fix typo on class name
    The class name was wrong, therefore it was not possible to load it from
    the defined entry point. This change fixes it.
    
    Fixes-Bug: 584636
    Change-Id: I9b32899a6c9f06da3f406d01c8181e6277a34896

--- keystoneauth1/loading/_plugins/identity/v3.py
+++ keystoneauth1/loading/_plugins/identity/v3.py
@@ -125,14 +125,14 @@ class OpenIDConnectAuthorizationCode(_OpenIDConnectBase):
         return options
 
 
-class OpenIDConnectAcessToken(loading.BaseFederationLoader):
+class OpenIDConnectAccessToken(loading.BaseFederationLoader):
 
     @property
     def plugin_class(self):
         return identity.V3OidcAccessToken
 
     def get_options(self):
-        options = super(OpenIDConnectAcessToken, self).get_options()
+        options = super(OpenIDConnectAccessToken, self).get_options()
 
         options.extend([
             loading.Opt('access-token', secret=True,
