f2cc77cba09039bb4be84655ae5de921c8e43859
ji-xuepeng <ji.xuepeng@zte.com.cn>
2016-07-10 13:28:43 +0800

    remove unused LOG
    This is to remove unused LOG to keep code clean.
    
    Change-Id: Iaf511b4d61590a9e014ab6a37a2c2b91c3916238

--- keystoneauth1/identity/generic/token.py
+++ keystoneauth1/identity/generic/token.py
@@ -10,13 +10,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from keystoneauth1 import _utils as utils
 from keystoneauth1 import discover
 from keystoneauth1.identity.generic import base
 from keystoneauth1.identity import v2
 from keystoneauth1.identity import v3
-
-LOG = utils.get_logger(__name__)
 
 
 class Token(base.BaseGenericPlugin):
