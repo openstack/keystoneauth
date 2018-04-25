5c79260971a87a5af002b3eeab82866928e88ac9
Monty Taylor <mordred@inaugust.com>
2018-04-25 19:10:40 +0200

    Infer version from old versioned service type aliases
    The last piece of service type alias support is to handle volumev2,
    volumev3, workflowv2, workflowv3 and friends.
    
    Although it's an annoying scenario, luckily legacy code that uses them
    has a clear meaning. volumev2, version='3' is just legit not a thing.
    
    Needed-By: https://review.openstack.org/564494
    Change-Id: Iec09bcb16d8e9b09e09bf12d03c2a55e679ad70c

--- keystoneauth1/exceptions/discovery.py
+++ keystoneauth1/exceptions/discovery.py
@@ -10,10 +10,17 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import os_service_types
+
 from keystoneauth1.exceptions import base
+
+_SERVICE_TYPES = os_service_types.ServiceTypes()
 
 
 __all__ = ('DiscoveryFailure',
+           'ImpliedVersionMismatch',
+           'ImpliedMinVersionMismatch',
+           'ImpliedMaxVersionMismatch',
            'VersionNotAvailable')
 
 
@@ -23,3 +30,28 @@ class DiscoveryFailure(base.ClientException):
 
 class VersionNotAvailable(DiscoveryFailure):
     message = "Discovery failed. Requested version is not available."
+
+
+class ImpliedVersionMismatch(ValueError):
+    label = 'version'
+
+    def __init__(self, service_type, implied, given):
+        super(ImpliedVersionMismatch, self).__init__(
+            "service_type {service_type} was given which implies"
+            " major API version {implied} but {label} of"
+            " {given} was also given. Please update your code"
+            " to use the official service_type {official_type}.".format(
+                service_type=service_type,
+                implied=str(implied[0]),
+                given=given,
+                label=self.label,
+                official_type=_SERVICE_TYPES.get_service_type(service_type),
+            ))
+
+
+class ImpliedMinVersionMismatch(ImpliedVersionMismatch):
+    label = 'min_version'
+
+
+class ImpliedMaxVersionMismatch(ImpliedVersionMismatch):
+    label = 'max_version'
