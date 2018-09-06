476d36df33f12bf432477c5d2954e40feccbbd0a
Gage Hugo <gagehugo@gmail.com>
2018-09-06 17:25:30 -0500

    Add nosec to instance of sha1
    This change adds nosec to the single instance of sha1 usage,
    which is only used for log hashing.
    
    Change-Id: Icf9fe6ee7cdbcbbd7f683f1baafdcc93e2429133

--- keystoneauth1/session.py
+++ keystoneauth1/session.py
@@ -318,7 +318,7 @@ class Session(object):
         secure_headers = ('authorization', 'x-auth-token',
                           'x-subject-token', 'x-service-token')
         if header[0].lower() in secure_headers:
-            token_hasher = hashlib.sha1()
+            token_hasher = hashlib.sha1()  # nosec log hashing
             token_hasher.update(header[1].encode('utf-8'))
             token_hash = token_hasher.hexdigest()
             return (header[0], '{SHA1}%s' % token_hash)
